d160756035673372b86bbc6a5e401f0dcf96d162
Nicolas <112377899+Programmerjavalearning@users.noreply.github.com>
2022-09-29 11:42:28 +0200

    Add files via upload

---
+++ SignupController.java
@@ -0,0 +1,51 @@
+package com.example.todoapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignupController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private CheckBox signUpCheckFemale;
+
+    @FXML
+    private CheckBox signUpCheckMale;
+
+    @FXML
+    private TextField signUpCountry;
+
+    @FXML
+    private TextField signUpFirstName;
+
+    @FXML
+    private TextField signUpLastName;
+
+    @FXML
+    private PasswordField signUpPassword;
+
+    @FXML
+    private TextField signUpUsername;
+
+    @FXML
+    void initialize() {
+
+
+
+
+    }
+}
